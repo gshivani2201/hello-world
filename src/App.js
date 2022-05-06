c852f2cdba8aef76c9dcaaef098b4df42beba8e7
Shivani Gupta <gupta.shivani7898@gmail.com>
2022-05-06 17:30:07 +0530

    Basics of form handling

--- src/App.js
+++ src/App.js
@@ -1,43 +1,12 @@
 import './App.css';
-//import React, {Component} from 'react'
-import Greet from './components/Greet'
-//import Hello from './components/Hello';
-import Welcome from './components/Welcome'
-import Message from './components/Message'
-import Counter from './components/Counter';
-import FunctionClick from './components/FunctionClick';
-import ClassClick from './components/ClassClick';
-import EventBind from './components/EventBind';
-import ParentComponent from './ParentComponent';
-import UserGreeting from './components/UserGreeting';
-import NameList from './components/NameList';
-import Stylesheet from './components/Stylesheet';
-import Inline from './components/Inline';
-import './appStyles.css'
-import styles from './appStyles.module.css'
+import React, {Component} from 'react'
+import Form from './components/Form';
+
 
 function App() {
   return (
     <div className="App">
-      <h1 className='error'>Error</h1>
-      <h1 className={styles.success}>Success</h1>
-    <Inline />  
-    {/* <Stylesheet primary={true}/> */}
-    {/* <NameList /> */}
-    {/* <UserGreeting /> */}
-      {/* <ParentComponent /> */}
-      {/* <EventBind /> */}
-      {/* <ClassClick /> */}
-      {/* <FunctionClick /> */}
-     {/* <Counter /> */}
-     {/* <Greet name="Tony Stark" heroName="Iron Man"/> 
-     <Greet name="Peter Parker" heroName="SpiderMan"/>
-     <Greet name="Natasha" heroName="Black Widow"/> */}
-     {/* <Welcome name="Tony Stark" heroName="Iron Man"/>
-     <Welcome name="Peter Parker" heroName="SpiderMan"/>
-     <Welcome name="Bruce" heroName="Hulk"/> */}
-     {/* <Message /> */}
-     {/* <Hello /> */}
+      <Form />
     </div>
   );
 }
