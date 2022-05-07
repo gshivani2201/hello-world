a79884f01abf112fe2ff4d2cd2d3a7637a5b1d29
Shivani Gupta <gupta.shivani7898@gmail.com>
2022-05-07 12:38:22 +0530

    updating lifecycle methods

--- src/components/LifecycleA.js
+++ src/components/LifecycleA.js
@@ -21,11 +21,32 @@ class LifecycleA extends Component {
         console.log('LifecycleA componentDidMount')
     }
 
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('LifecycleA shouldComponentUpdate')
+        return true
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log('LifecycleA getSnapshotBeforeUpdate')
+        return null
+    }
+
+    componentDidUpdate() {
+        console.log('LifecycleA componentDidUpdate')
+    }
+
+    changeState = () => {
+        this.setState({
+            name: 'Developer'
+        })
+    }
+
   render() {
     console.log('LifecycleA render')
     return (
       <div>
-          Lifecycle A
+          <div>Lifecycle A</div>
+          <button onClick={this.changeState}>Change state</button>
           <LifecycleB />
       </div>
     )
