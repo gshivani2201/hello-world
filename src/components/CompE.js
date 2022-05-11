c1338126a1044ea741c600a5fdbc96bd525f1bed
Shivani Gupta <gupta.shivani7898@gmail.com>
2022-05-11 13:13:43 +0530

    context P-2

--- src/components/CompE.js
+++ src/components/CompE.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
 import CompF from './CompF'
+import UserContext from './userContext'
 
 class CompE extends Component {
+
+    static contextType = UserContext
+    
   render() {
-    return <CompF />
+    return (
+        <div>
+            CompE context {this.context}
+            <CompF />
+        </div>
+    ) 
   }
 }
+
 
 export default CompE
