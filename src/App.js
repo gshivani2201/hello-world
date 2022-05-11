7b948591fa412beab892dca2f70132a34ec2dc37
Shivani Gupta <gupta.shivani7898@gmail.com>
2022-05-11 12:31:43 +0530

    context P-1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import './App.css'
-import ClickCounterTwo from './components/ClickCounterTwo';
-import HoverCounterTwo from './components/HoverCounterTwo';
-import CounterTwo from './components/CounterTwo'
+import CompC from './components/CompC';
+import { UserProvider } from './components/userContext';
 
 
 function App() {
   return (
     <div className='App'>
-      <CounterTwo 
-        render={(count, incrementCount) => (
-          <ClickCounterTwo count={count} incrementCount={incrementCount} />
-        )}  
-      />
-      <CounterTwo 
-        render={(count, incrementCount) => (
-          <HoverCounterTwo count={count} incrementCount={incrementCount} />
-        )}  
-      />
-      {/* <ClickCounterTwo />
-      <HoverCounterTwo /> */}
+      <UserProvider value="Shivani">
+        <CompC />
+      </UserProvider>
     </div>
   );
 }
